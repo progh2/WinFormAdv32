e2feb0f93df3c0ad5af04274f57dd89e14df8ea4
Gihun Ham <ham@e-mirim.hs.kr>
2024-08-20 09:07:28 +0900

    서식 서브메뉴 작성

--- WinFormAdv32/Form1.Designer.cs
+++ WinFormAdv32/Form1.Designer.cs
@@ -60,6 +60,8 @@
             this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
             this.모두선택ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.시간날짜ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.자동줄바꿈ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.글꼴ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -131,6 +133,9 @@
             // 
             // 서식ToolStripMenuItem
             // 
+            this.서식ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.자동줄바꿈ToolStripMenuItem,
+            this.글꼴ToolStripMenuItem});
             this.서식ToolStripMenuItem.Name = "서식ToolStripMenuItem";
             this.서식ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.서식ToolStripMenuItem.Text = "서식";
@@ -286,6 +291,18 @@
             this.시간날짜ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.시간날짜ToolStripMenuItem.Text = "시간/날짜";
             // 
+            // 자동줄바꿈ToolStripMenuItem
+            // 
+            this.자동줄바꿈ToolStripMenuItem.Name = "자동줄바꿈ToolStripMenuItem";
+            this.자동줄바꿈ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.자동줄바꿈ToolStripMenuItem.Text = "자동 줄 바꿈";
+            // 
+            // 글꼴ToolStripMenuItem
+            // 
+            this.글꼴ToolStripMenuItem.Name = "글꼴ToolStripMenuItem";
+            this.글꼴ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.글꼴ToolStripMenuItem.Text = "글꼴";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -336,6 +353,8 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
         private System.Windows.Forms.ToolStripMenuItem 모두선택ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 시간날짜ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 자동줄바꿈ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 글꼴ToolStripMenuItem;
     }
 }
 
